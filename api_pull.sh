9076f3b3a545a64d08cd969f19e799ddb0e009e2
LaexTheCat <dogukan.met@gmail.com>
2024-03-02 12:54:37 +0100

    additional endpoints for contributors and events

--- api_pull.sh
+++ api_pull.sh
@@ -2,3 +2,5 @@ mkdir $1_$2
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/pulls > $1_$2/pulls.txt
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/commits > $1_$2/commits.txt
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/issues > $1_$2/issues.txt
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/events > $1_$2/events.txt
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/contributors > $1_$2/contributors.txt
