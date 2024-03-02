694abee2d62236fc452aa1f8cd2eec1bca75878b
LaexTheCat <dogukan.met@gmail.com>
2024-03-02 12:25:46 +0100

    Made script to pull commits issues and pulls from a sample repo

---
+++ api_pull.sh
@@ -0,0 +1,3 @@
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/GeyserMC/Geyser/pulls > pulls.txt
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/GeyserMC/Geyser/commits > commits.txt
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/GeyserMC/Geyser/issues > issues.txt
