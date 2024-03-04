f71ca3b59afb7785b1faede8053e5ab12d1286d8
Mohammad Al Shakoush <mo.alshakoush@gmail.com>
2024-03-04 09:43:43 +0100

    Use JSON in api_pull

--- api_pull.sh
+++ api_pull.sh
@@ -1,6 +1,6 @@
 mkdir $1_$2
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/pulls > $1_$2/pulls.txt
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/commits > $1_$2/commits.txt
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/issues > $1_$2/issues.txt
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/events > $1_$2/events.txt
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/contributors > $1_$2/contributors.txt
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/pulls > $1_$2/pulls.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/commits > $1_$2/commits.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/issues > $1_$2/issues.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/events > $1_$2/events.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$1/$2/contributors > $1_$2/contributors.json
